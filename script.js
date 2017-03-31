4eda97d10ed69564e961eaa2a1dd976a5412e5b3
aiiins <mcgrath.ainsley@gmail.com>
2017-03-31 13:57:40 -0500

    add show/hide, but it only works one way

---
+++ script.js
@@ -0,0 +1,7 @@
+// show/hide menu
+var button = document.getElementById("john");
+var menu = document.getElementById("menu_img");
+
+button.onclick = function() {
+  menu_img.style.display = "block";
+}
