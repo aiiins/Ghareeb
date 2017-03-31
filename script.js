9a9bda046070aa7c9988703cc652bc0fdda3f19f
aiiins <mcgrath.ainsley@gmail.com>
2017-03-31 14:33:36 -0500

    script.js stops working..?

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var button = document.getElementById("john");
 var menu = document.getElementById("menu_img");
 
 button.onclick = function() {
-  if (menu.style.display != "block") {
+  if (menu.style.display !== "block") {
     menu.style.display = "block"
   } else {
     menu.style.display = "none"
