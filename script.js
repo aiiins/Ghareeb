bbdb1060e3c79afed24b0120ac3e377ef1c4fe8f
aiiins <mcgrath.ainsley@gmail.com>
2017-04-11 21:17:10 -0500

    ix page flow, fix button, clean up css, beautify everything

--- script.js
+++ script.js
@@ -3,18 +3,18 @@
 var button = document.getElementById("john");
 
 var imgArray = {
-"achar-ghost": "achar-ghost.jpg",
-"aloo-palak": "aloo-palak-2.jpg"
+    "achar-ghost": "achar-ghost.jpg",
+    "aloo-palak": "aloo-palak-2.jpg"
 }
 
 button.onclick = function() {
-  var menu = document.getElementById("menu_img");
-  
-  if (menu.style.display !== "block") {
-    menu.style.display = "block"
-  } else {
-    menu.style.display = "none"
-  }
+    var menu = document.getElementById("menu-img-box");
+
+    if (menu.style.display !== "flex") {
+        menu.style.display = "flex"
+    } else {
+        menu.style.display = "none"
+    }
 }
 
 // needs to be resversed
